3099276b754d30305d53ab16f7d4d2b4098e62cf
JangSoyun <soyoon5328@gmail.com>
2024-02-14 22:38:58 +0900

    feat: select tab

--- src/youtube_scrapper.py
+++ src/youtube_scrapper.py
@@ -9,6 +9,20 @@ chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(options=chrome_options)
 
-def getChannel(url:str):
+def get_channel(url:str):
     driver.get(url)
 
+class Tab(Enum):
+    홈='홈'
+    동영상='동영상'
+    Shorts='Shorts'
+    라이브='라이브'
+    재생목록='재생목록'
+    커뮤니티='커뮤니티'
+    멤버십='멤버십'
+
+def select_tab(tab_name:Tab):
+    element=driver.find_element(by=webdriver.common.by.By.CSS_SELECTOR,value='[tab-title="홈"]')
+    if(element==None) :
+        print(f'error : no element named ${tab_name}')
+    return element
